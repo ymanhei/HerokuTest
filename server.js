8b231cb0f6fd9f341da9c6d70c7f6199895d496d
Shing Chan <ymanhei@gmail.com>
2019-12-07 13:53:16 +0800

    Test

--- server.js
+++ server.js
@@ -89,6 +89,6 @@ app.post("/api/characters", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT || 5000, function() {
+app.listen(PORT || process.env.PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
